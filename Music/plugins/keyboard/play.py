18421004f057fd9ec5fe45b80b0b5dadf16e53c5
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-03 22:00:13 +0530

    Update play.py

--- Music/plugins/keyboard/play.py
+++ Music/plugins/keyboard/play.py
@@ -19,31 +19,31 @@ def play_markup(_, chat_id):
     return buttons
 
 
-def next_markup(_, chat_id):
-    buttons = [
-         [
-            InlineKeyboardButton(
-                 text="15%", 
-                 callback_data="vol1"), 
-            InlineKeyboardButton(
-                 text="50%", 
-                 callback_data="vol2"), 
-         ], 
-         [
-            InlineKeyboardButton(
-                 text="75%", 
-                 callback_query="vol3"), 
-            InlineKeyboardButton(
-                  text="100%", 
-                  callback_query="vol4")
-         ], 
-         [
-            InlineKeyboardButton(
-                  text="Close",
-                  callback_data="close") 
-         ], 
-      ]
-      return buttons
+# for updatedef next_markup(_, chat_id):
+#    buttons = [
+#         [
+#           InlineKeyboardButton(
+#                text="15%", 
+#                 callback_data="vol1"), 
+#            InlineKeyboardButton(
+#                 text="50%", 
+#                 callback_data="vol2"), 
+#         ], 
+#         [
+#            InlineKeyboardButton(
+#                 text="75%", 
+#                 callback_query="vol3"), 
+#            InlineKeyboardButton(
+#                  text="100%", 
+#                  callback_query="vol4")
+#         ], 
+#         [
+#            InlineKeyboardButton(
+#                  text="Close",
+#                  callback_data="close") 
+#         ], 
+#      ]
+#      return buttons
 
 def menu_markup(_, chat_id):
     buttons = [
