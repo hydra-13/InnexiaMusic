c930b205b045ef3b61901e6e6530a00824e084fb
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-03 21:59:00 +0530

    Update callbacks.py

--- Music/plugins/callbacks.py
+++ Music/plugins/callbacks.py
@@ -2,7 +2,7 @@ import yt_dlp
 from Music.Client.queues import QUEUE
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup)
-from Music.plugins.keyboard.play import play_markup, next_markup 
+from Music.plugins.keyboard.play import play_markup, next_markup, menu_markup, panel_markup
 
 @Client.on_callback_query(filters.regex("close"))
 async def close(_, query: CallbackQuery):
@@ -14,53 +14,24 @@ async def menu(_, query: CallbackQuery):
      a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
     await query.edit_message_text(
         "**Welcome To Menu Of MusicBot :**",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Panel🎛", callback_data="panel"),
-                    InlineKeyboardButton("Lyrics🔎", callback_data="lyrics"),
-                ],
-                [
-                    InlineKeyboardButton ("Support⛓", callback="back"),          
-                    InlineKeyboardButton("🗑 Close", callback_data="close")
-                ],
-           ]
-        ),
-    )
+        reply_markup=InlineKeyboardMarkup(mdnu_markup), 
+   ) 
 
 @Client.on_callback_query(filters.regex("panel")) 
 async def panel(_, query: CallbackQuery):
      a = await _.get_chat_member(query.message.chat.id, query.from_user.id) 
     await query.edit_message_text(
         "**Here  Is Control Panel Of Music**", 
-        reply_markup=InlineKeyboardMarkup(
-            [
-                 [
-                     InlineKeyboardButton("⏸", callback_data="pause"),
-                     InlineKeyboardButton("⏹", callback_data="stop"), 
-                     InlineKeyboardButton("▶️", callback_data="play"), 
-                 ], 
-                 [
-                     InlineKeyboardButton("🔇", callback_data="mute"), 
-                     InlineKeyboardButton("🔊", callback_data="unmute"),
-                 ], 
-                 [
-                     InlineKeyboardButton("Next", callback_data="next"), 
-                     InlineKeyboardButton("🔙", callback_data="menu"), 
-                 ], 
-                 [
-                     InlineKeyboardButton("🗑Close", callbackdata="close")
-                 ]
-            ]
-       ),
+        reply_markup=InlineKeyboardMarkup(panel_markup), 
     ) 
-@Client.on_callback_query(filters.regex("next")) 
-async def next(_, query: CallbackQuery):
-    a = await_.get_chat_member(query.message.chat.id, query.from_user.id) 
-   await query.edit_message_text(
-       "**Here  Is Control Panel Of Music**",
-       reply_markup=InlineKeyboardButton(next_markup) 
-     )
+
+# @Client.on_callback_query(filters.regex("next")) 
+# async def next(_, query: CallbackQuery):
+#    a = await_.get_chat_member(query.message.chat.id, query.from_user.id) 
+#   await query.edit_message_text(
+ #      "**Here  Is Control Panel Of Music**",
+#       reply_markup=InlineKeyboardButton(next_markup), 
+#     )
 
 @Client.on_callback_query(filters.regex("play"))
 async def on_resumeTrue(_, CallbackQuery):  
