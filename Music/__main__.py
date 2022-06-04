d5bf8953d77094f619eda18eaee9ed9d9483f9cc
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-04 07:31:20 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,7 +1,7 @@
 import asyncio
 from pytgcalls import idle
 from Music import LOG_CHAT
-from Music.Client.tgcalls import call_py, Bot, sex as abhi
+from Music.Client.tgcalls import Mikki, Bot, sex 
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
@@ -13,15 +13,17 @@ async def start_bot():
 
     print("[INFO]: STARTING PYTGCALLS CLIENT")
 
-    await call_py.start()
-    
-    await abhi.send_message(
+    await Mikki.start()
+    await idle()
+    await sex.send_message(
                             LOGGER,
                             "<b>Innexia Music Assistant Successfully Started!</b>"
     ), 
-    await abhi.join_chat("RexomaSupport"), 
-    await abhi.join_chat("RexomaUpdate") 
+    await sex.join_chat("RexomaSupport"), 
+    await sex.join_chat("RexomaUpdate") 
     print("[INFO]: STOPPING BOT & USERBOT")    
     await Bot.stop()
 
-await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot()) 
