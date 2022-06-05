76429dca97f9759d73ad96c1b9977e42b831452b
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-05 13:53:22 +0530

    Update Unmute.py

--- Music/plugins/Admins/Unmute.py
+++ Music/plugins/Admins/Unmute.py
@@ -1,1 +1,20 @@
+from pyrogram import Client, filters
+from pytgcalls.exceptions import GroupCallNotFound
+from Music import Mikki
 
+
+@Client.on_message(filters.command("unmute"))
+
+async def resume(client, message):
+    query = " ".join(message.command[1:])
+    if query == "channel":
+        chat_id = int(message.chat.title)
+        type = "Channel"
+    else:
+        chat_id = message.chat.id
+        type = "Group"
+    try:
+        await Mikki.unmute_stream(chat_id)
+        await message.reply(f"**{type} stream Resumed**")
+    except GroupCallNotFound:
+        await message.reply("**Error:** GroupCall not found!")
