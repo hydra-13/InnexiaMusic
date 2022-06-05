5cb018704fa33d4d59563a294017afccd0cec5bd
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-05 13:42:41 +0530

    Update Pause.py

--- Music/plugins/Admins/Pause.py
+++ Music/plugins/Admins/Pause.py
@@ -1,1 +1,20 @@
+from pyrogram import Client, filters
+from pytgcalls.exceptions import GroupCallNotFound
+from Music import Mikki
 
+
+@Client.on_message(filters.command("pause"))
+
+async def pause(client, message):
+    query = " ".join(message.command[1:])
+    if query == "channel":
+        chat_id = int(message.chat.title)
+        type = "Channel"
+    else:
+        chat_id = message.chat.id
+        type = "Group"
+    try:
+        await Mikki.pause_stream(chat_id)
+        await message.reply(f"**{type} stream paused!**")
+    except GroupCallNotFound:
+        await message.reply("**Error:** GroupCall not found!")
