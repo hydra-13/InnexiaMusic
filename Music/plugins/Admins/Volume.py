0fb3e8e8b6305b16e10d93e893f2e64c0e397064
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-05 13:50:10 +0530

    Update Volume.py

--- Music/plugins/Admins/Volume.py
+++ Music/plugins/Admins/Volume.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters
+from Music import Mikki
 
+
+@Client.on_message(filters.command("volume"))
+
+async def change_volume(client, message):
+    range = message.command[1]
+    chat_id = message.chat.id
+    try:
+        await Mikki.change_volume_call(chat_id, volume=int(range))
+        await message.reply(f"**Volume changed to:** `{range}%`")
+    except Exception as e:
+        await message.reply(f"**Error:** {e}")
