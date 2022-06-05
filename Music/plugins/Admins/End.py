c868149af1f79d4fd69ca1fec718c8bf3553951f
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-05 13:48:02 +0530

    Update End.py

--- Music/plugins/Admins/End.py
+++ Music/plugins/Admins/End.py
@@ -1,1 +1,20 @@
+from pyrogram import Client, filters
+from pytgcalls.exceptions import GroupCallNotFound
+from Music import Mikki
 
+
+@Client.on_message(filters.command(["end", "stop"]))
+
+async def end(client, message):
+    query = " ".join(message.command[1:])
+    if query == "channel":
+        chat_id = int(message.chat.title)
+        type = "Channel"
+    else:
+        chat_id = message.chat.id
+        type = "Group"
+    try:
+        await Mikki.leave_group_call(chat_id)
+        await message.reply(f"**{type} stream Ended!**")
+    except GroupCallNotFound:
+        await message.reply("**Error:** GroupCall not found!")
