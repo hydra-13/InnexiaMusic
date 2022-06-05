fa14fac7ae9a7d1abb9bb43be3b6ad0cc513f96e
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-05 14:25:29 +0530

    Update __init__.py

--- Music/plugins/Admins/__init__.py
+++ Music/plugins/Admins/__init__.py
@@ -1,1 +1,7 @@
 from .End import end
+from .Skip import skip
+from .Mute import mute
+from .Unmute import unmute
+from .Pause import pause
+from .Resume import resume
+from .Volume import change_volume
